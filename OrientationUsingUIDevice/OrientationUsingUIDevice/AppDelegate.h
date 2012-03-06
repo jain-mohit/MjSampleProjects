16378282d82102be66e3660b461f64a54952b884
Mohit Jain <mohit.jain@compuware.com>
2012-03-06 14:34:32 -0500

    Adding sample project for orientation using UIDevice

---
+++ OrientationUsingUIDevice/OrientationUsingUIDevice/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  OrientationUsingUIDevice
+//
+//  Created by Mohit Jain on 3/6/12.
+//  Copyright (c) 2012 Compuware. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
