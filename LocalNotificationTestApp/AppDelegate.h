dd2f1367e64b69597f5da3874b0c358e22274ffa
Mohit Jain <mohit.jain@compuware.com>
2012-03-15 13:57:06 -0400

    Initial Commit

---
+++ LocalNotificationTestApp/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  LocalNotificationTestApp
+//
+//  Created by Mohit Jain on 3/15/12.
+//  Copyright (c) 2012 Compuware. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ LocalNotificationTestApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  LocalNotificationTestApp
+//
+//  Created by Mohit Jain on 3/15/12.
+//  Copyright (c) 2012 Compuware. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
